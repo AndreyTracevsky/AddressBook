877064f7ce9a5a82fcb7284ebc8d15ba63eba7c6
Andrey <trac-andrej@ya.ru>
2019-10-15 15:17:46 +0300

    Все вспомогательные методы перенесены в файл application.py. В тестовых методах остались только тесты). Код РАБОТАЕТ!!!:)

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,88 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+from appication import Application
+import pytest
 from contact import Contact
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_contact_page(wd)
-        self.create_contact(wd, Contact(firstname = "Andrey", midlename = "Vladimirovich", lastname = "Tracevskij", nickname = "Zverun",
-                            title = "QA", company = "Adani",
-                            address = "Novodvorskij s/s 116", phone_home = "80172234522", phone_mobile = "8044777777",
-                            phone_work = "2222222", email = "test@ya.ru"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_contact_page(wd)
-        self.create_contact(wd, Contact(firstname = "", midlename = "", lastname = "", nickname = "", title = "", company = "",
-                            address = "", phone_home = "", phone_mobile = "", phone_work = "", email = ""))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, wd, contact):
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.midlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.phone_home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.phone_mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.phone_work)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def open_contact_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(firstname = "Andrey", midlename = "Vladimirovich", lastname = "Tracevskij", nickname = "Zverun",
+                        title = "QA", company = "Adani", address = "Novodvorskij s/s 116", phone_home = "80172234522", phone_mobile = "8044777777", phone_work = "2222222", email = "test@ya.ru"))
+    app.logout()
+
+
+def test_add_empty_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(firstname = "", midlename = "", lastname = "", nickname = "", title = "", company = "", address = "", phone_home = "", phone_mobile = "", phone_work = "", email = ""))
+    app.logout()
