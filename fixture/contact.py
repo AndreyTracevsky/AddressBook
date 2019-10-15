1563f409bd6c7f8f1d92b17d5bd622802b6ebf3d
Andrey <trac-andrej@ya.ru>
2019-10-15 16:44:10 +0300

    Я заставил код работать!!! Перенес вспомогательные методы для работы с группами в файл group.py который расположен в пакете fixture, также были перенесены вспомогательные методы для работы с контактами в файл contact.py который расположен в пакете fixture. Все работает, продолжение следует...

---
+++ fixture/contact.py
@@ -0,0 +1,46 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create_contact(self, contact):
+        wd = self.app.wd
+        self.open_contact_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.midlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.phone_home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.phone_mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.phone_work)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from model.contact import Contact
 from fixture.appication import Application
 import pytest
-from model.contact import Contact
+
+
 
 @pytest.fixture()
 def app(request):
@@ -12,12 +14,12 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login("admin", "secret")
-    app.create_contact(Contact(firstname = "Andrey", midlename = "Vladimirovich", lastname = "Tracevskij", nickname = "Zverun",
+    app.contact.create_contact(Contact(firstname = "Andrey", midlename = "Vladimirovich", lastname = "Tracevskij", nickname = "Zverun",
                         title = "QA", company = "Adani", address = "Novodvorskij s/s 116", phone_home = "80172234522", phone_mobile = "8044777777", phone_work = "2222222", email = "test@ya.ru"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login("admin", "secret")
-    app.create_contact(Contact(firstname = "", midlename = "", lastname = "", nickname = "", title = "", company = "", address = "", phone_home = "", phone_mobile = "", phone_work = "", email = ""))
+    app.contact.create_contact(Contact(firstname = "", midlename = "", lastname = "", nickname = "", title = "", company = "", address = "", phone_home = "", phone_mobile = "", phone_work = "", email = ""))
     app.session.logout()
