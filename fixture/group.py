9bf482a7fcaddc5b462015c438e890062f9484cd
Admin <trac-andrej@ya.ru>
2020-01-14 15:26:23 +0300

    Урок 4_1: Научились загружать список групп и пользовательского web-приложения. В тесты добавились проверки которые сравнивают список групп до того как добавили/удалили или модифицировали группу и после того как выполнили действие добавления/удаления или модификации

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -61,3 +63,13 @@ class GroupHelper:
     def return_to_home_page(self):
         wd = self.app.wd
         wd.get("http://localhost/addressbook/group.php")
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name = text, id = id))
+        return groups
