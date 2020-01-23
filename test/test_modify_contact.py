a35ee8cf6f81bf84a94f1e93d01fae22d09b60ed
Admin <trac-andrej@ya.ru>
2020-01-23 07:53:09 +0300

    Урок 3_4 Добавил проверку условия. Проверяется наличие хотябы одной группы для удаления, если группы нет, то сначала ее создаем, а затем удаляем

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,6 +7,7 @@ def test_modify_contact_firstname(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
 
+
 def test_modify_contact_middlename(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(middlename = "Vasilievich"))
@@ -20,11 +21,13 @@ def test_modify_contact_lastname(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
 
+
 def test_modify_contact_nickname(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(nickname = "ZverOK"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+
 
 def test_modify_contact_title(app):
     old_contacts = app.contact.get_contact_list()
@@ -32,11 +35,13 @@ def test_modify_contact_title(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
 
+
 def test_modify_contact_company(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(company = "Nike"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+
 
 def test_modify_contact_address(app):
     old_contacts = app.contact.get_contact_list()
@@ -44,11 +49,13 @@ def test_modify_contact_address(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
 
+
 def test_modify_contact_phone_home(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(phone_home = "80171111111"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+
 
 def test_modify_contact_phone_mobile(app):
     old_contacts = app.contact.get_contact_list()
@@ -56,11 +63,13 @@ def test_modify_contact_phone_mobile(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
 
+
 def test_modify_contact_phone_work(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(phone_work = "80443333333"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+
 
 def test_modify_contact_email(app):
     old_contacts = app.contact.get_contact_list()
