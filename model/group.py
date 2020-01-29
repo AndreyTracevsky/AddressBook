8f27fb333ee18956c9a272c04b948aaec929e60e
tracevsky <trac-andrek@ya.ru>
2020-01-29 13:55:04 +0300

    Добавили логическое сравнение двух списков, теперь они сраваниваются по содержанию, а не по размеру.

--- model/group.py
+++ model/group.py
@@ -5,3 +5,9 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self): # функция которая определяет как должен выглядеть объект при выводе на консоль
+        return "%s:%s" % (self.id, self.name) # вывод двух значений (идендификатор и имя)
+
+    def __eq__(self, other): # функция "логического сравнения", сравниваются идендификаторы и имена в двух списках
+        return self.id == other.id and self.name == other.name # выполнение сравнения объектов по смыслу (логическое сравнение)
